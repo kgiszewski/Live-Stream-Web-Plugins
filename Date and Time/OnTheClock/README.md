9b0a7aac3dd924fe83665bbe4381908dfc0a2de1
kgiszewski <me@giszewski.com>
2018-01-26 13:17:17 -0500

    Start OnTheClock

---
+++ Date and Time/OnTheClock/README.md
@@ -0,0 +1,13 @@
+# Countdown Timer
+A simple plugin powered by Moment.js and Coundown.js.
+
+Highly customizable, set the date in the future or just add a few seconds from now.
+
+Output formatting is a matter of options passed into the args.
+
+Currently setup to stop at zero, though you can rework it to count back up after the time has passed.
+
+![thumb](img/thumb.gif)
+
+Hate the style? Update the CSS (including font and color).
+
---
+++ Date and Time/OnTheClock/js/app.js
@@ -0,0 +1,38 @@
+var app = (function () {
+	
+	var startTimeInSeconds = 60;
+	
+	var seconds;
+	
+	var start = function(){
+		seconds = startTimeInSeconds;
+		
+		startCountdown();
+	}
+	
+	var updateClock = function() {
+		seconds--;
+		
+		if(seconds < 0) {
+			seconds = startTimeInSeconds;
+		}
+		
+		var secondsLabel = (seconds > 9) ? seconds: "0" + seconds;
+		
+		$("h1 > span").text(secondsLabel); 
+	}
+	
+		
+	var startCountdown = function() {
+		
+		var handle = setInterval(updateClock, 1000);
+		
+		return handle;
+	}
+	
+	return {
+		start: start
+	}		
+})();
+
+app.start();
