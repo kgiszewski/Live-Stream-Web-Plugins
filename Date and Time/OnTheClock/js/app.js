5c18e40fe97ba635dc47f1bfbb06af32c280563c
kgiszewski <me@giszewski.com>
2018-01-27 16:42:51 -0500

    Move app code to app.js

--- Date and Time/OnTheClock/js/app.js
+++ Date and Time/OnTheClock/js/app.js
@@ -1,34 +1,42 @@
 var app = (function () {
 	
-	var startTimeInSeconds = 60;
+	var clock;		
+	var initialTime = 90;
+	var showYellow = 45;
+	var showRed = 15;
 	
-	var seconds;
-	
-	var start = function(){
-		seconds = startTimeInSeconds;
+	$(document).ready(function() {
+		var time = 0;
 		
-		startCountdown();
-	}
-	
-	var updateClock = function() {
-		seconds--;
+		var $clock = $('.clock');
+		var $clockWrapper = $('#onClock');
 		
-		if(seconds < 0) {
-			seconds = startTimeInSeconds;
-		}
-		
-		var secondsLabel = (seconds > 9) ? seconds: "0" + seconds;
-		
-		$(".clock").text(secondsLabel); 
-	}
-	
-		
-	var startCountdown = function() {
-		
-		var handle = setInterval(updateClock, 1000);
-		
-		return handle;
-	}
+		clock = new FlipClock($clock, initialTime, {
+			// Create a minute counter
+			clockFace: 'MinuteCounter',
+			countdown: true,
+			callbacks: {
+				stop: function() {
+					$clockWrapper.removeClass("red");						
+					clock.setTime(initialTime + 1);
+					clock.start();	
+				},
+				interval: function() {	
+					if(clock){
+						var time = clock.time.time;
+						
+						if(time > showYellow){							
+							$clockWrapper.addClass("green").removeClass("red");
+						} else if(time == showYellow){
+							$clockWrapper.removeClass("green").addClass("yellow");
+						} else if(time == showRed){
+							$clockWrapper.removeClass("yellow").addClass("red");
+						}							
+					}											
+				}					
+			}				
+		});
+	});
 	
 	return {
 		start: start
